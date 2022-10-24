815a8e403271ccb2b264d9c5c1a176a663f1fbe2
Duy Khánh <18130107@st.hcmuaf.edu.vn>
2022-10-24 20:04:10 +0700

    Thêm modal popup cho nguoi dung tim tai khoan bang mail

---
+++ js/forgot-password.js
@@ -0,0 +1,9 @@
+// let modal = document.querySelector('#modal');
+// let btn = document.getElementById("forget-password-button");
+//
+// function toggle(){
+// modal.style.display = 'block';
+// }
+// btn.addEventListener('click',toggle);
+//
+//
