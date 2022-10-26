21916f0451f02b0aa41fa1446e22fed60f891504
Duy Khánh <18130107@st.hcmuaf.edu.vn>
2022-10-26 11:22:42 +0700

    Sua loi modal bi bien mat

--- js/forgot-password.js
+++ js/forgot-password.js
@@ -1,9 +1,19 @@
-// let modal = document.querySelector('#modal');
-// let btn = document.getElementById("forget-password-button");
-//
-// function toggle(){
-// modal.style.display = 'block';
-// }
-// btn.addEventListener('click',toggle);
-//
+let modal = document.querySelector('#modal');
+let bodyBackgroud = document.querySelector('.hidden-body-background');
+let btn = document.getElementById("forget-password-button");
+
+function toggle(event){
+
+    event.preventDefault()
+    modal.style.display = 'block';
+
+
+}
+
+btn.addEventListener('click',toggle);
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 //
